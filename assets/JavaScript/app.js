d2ccf8b6e030e70b37501808493571f7d6c0929d
Justin Lamb <lambjg63@gmail.com>
2018-11-15 17:54:27 -0500

    Folders and skeleton done

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,18 @@
+var timer;
+var question1;
+var question2;
+var question3;
+var question4;
+var winnerLoser;
+
+function timer() {
+   //this function needs to begin the timer countdown 
+}
+
+function questions() {
+    //this function needs to see if the answers picked are correct
+}
+
+function timesup() {
+    //this needs to check the answers at the end of the time and declare a winner or loser
+}
