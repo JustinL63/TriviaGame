c43582216cdb92ac81f31ac20249ec939370b716
Justin Lamb <lambjg63@gmail.com>
2018-11-15 19:17:37 -0500

    Meh

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,13 +1,34 @@
-var timer;
-var question1;
-var question2;
-var question3;
-var question4;
+var timer = 30;
+var intervalId;
+var timerRunning = false;
 var winnerLoser;
+var questions = [
+    {
+        question: "Who is Batman?",
+        answer: "Bruce Wayne"
 
-function timer() {
-   //this function needs to begin the timer countdown 
+    },
+    {
+        question: "What was Luke Skywalker before he was a Jedi?",
+        answer: "Moisture Farmer"
+    }
+]
+//need a function to take time off the timer
+function countdown() {
+   (timer === timer--);
 }
+
+function timerStart() {
+   //this function needs to begin the timer countdown 
+    
+     intervalId = setInterval(countdown, 1000);
+    //   clearInterval(intervalId);
+     console.log(timer);
+     
+}
+$("#button").on("click", timerStart(timer))
+console.log(timer);
+
 
 function questions() {
     //this function needs to see if the answers picked are correct
