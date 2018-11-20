1bb3a75d6cd3d844e5631bd4ef798b91b2a0fcc3
Justin Lamb <lambjg63@gmail.com>
2018-11-19 20:47:11 -0500

    still trying

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -2,7 +2,7 @@ var timer = 30;
 var intervalId;
 var timerRunning = false;
 var winnerLoser;
-var score =0;
+var score = 0;
 
 //need a function to take time off the timer
 function countdown() {
@@ -22,20 +22,33 @@ function timerStart() {
     console.log(timer);
 
 }//I need this function to being the countdown on a button push
-$("#timerButton").on("click", countdown(timer))
-    ;
+$("#timerButton").on("click", countdown(timer));
 
-
-
-function questionsCheck() {
+function questionsCheck(questions, value) {
     if ("#question1" === value3) {
         score++;
-
     }
+    if ("#question2" === value2) {
+        score++;
+    }
+    if ("#question3" === value4) {
+        score++;
+    }
+    if ("#question4" === value2) {
+        score++;
+    }
+    if ("#question5" === value3) {
+        score++;
+    }
+    if ("#question6" === value2) {
+        score++;
+    }
+    alert("You got " + score + "out of 6!")
 
     //this function needs to see if the answers picked are correct
+};
+$("#submit").on("click").questionsCheck();
 
-}
 
 function timesup() {
     //this needs to check the answers at the end of the time and declare a winner or loser
