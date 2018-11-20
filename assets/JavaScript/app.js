9a368d5c54f37b68fc7e629cde28e5d0b7b1f720
Justin Lamb <lambjg63@gmail.com>
2018-11-19 20:34:50 -0500

    not great

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -2,31 +2,37 @@ var timer = 30;
 var intervalId;
 var timerRunning = false;
 var winnerLoser;
+var score =0;
 
 //need a function to take time off the timer
 function countdown() {
-  var timeLeft = (timer == timer--);
+    var timeLeft = (timer == timer--);
 
-setInterval(function(){countdown,1000});
+    setInterval(function () { timeLeft, 1000 });
 
-console.log(timeLeft);
+    console.log(timeLeft);
 };
 
 
 function timerStart() {
-   //this function needs to begin the timer countdown 
-    
-     intervalId = setInterval(countdown, 1000);
+    //this function needs to begin the timer countdown 
+
+    intervalId = setInterval(countdown, 1000);
     //   clearInterval(intervalId);
-     console.log(timer);
-     
+    console.log(timer);
+
 }//I need this function to being the countdown on a button push
 $("#timerButton").on("click", countdown(timer))
-;
+    ;
 
 
 
-function questions() {
+function questionsCheck() {
+    if ("#question1" === value3) {
+        score++;
+
+    }
+
     //this function needs to see if the answers picked are correct
 
 }
